fd4105b36714bee306c263ee2c99be6ac2b0d3bc
e0c6a37014 <pbta1@cryptolab.net>
2023-05-24 17:13:48 +0200

    feat(keymap): adjust game layers

--- keymap.c
+++ keymap.c
@@ -134,25 +134,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //Game
    [6] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------------------------.                            ,-----------------------------------------------------------------------.
-          KC_TAB,       KC_Q,       KC_W,       KC_E,       KC_T,       KC_Y,                                 XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_BSPC,
+          KC_TAB,       KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                    KC_Y,       KC_U,       KC_I,       KC_O,      KC_P,     KC_BSPC,
   //|-----------+-----------+-----------+-----------+-----------+-----------|                            |-----------+-----------+-----------+-----------+-----------+-----------|
-         KC_LSFT,       KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                 XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+         KC_LCTL,       KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                    KC_H,       KC_J,       KC_K,       KC_L,    KC_SCLN,    KC_QUOT,
   //|-----------+-----------+-----------+-----------+-----------+-----------|                            |-----------+-----------+-----------+-----------+-----------+-----------|
-         KC_LCTL,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                                 XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     KC_ESC,
+         KC_LSFT,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                                    KC_N,       KC_M,    KC_COMM,     KC_DOT,    KC_SLSH,     KC_ESC,
   //|-----------+-----------+-----------+-----------+-----------+-----------+-----------|    |-----------+-----------+-----------+-----------+-----------+-----------+-----------|
-                                                         KC_LALT,LT(7,KC_SPC),  KC_RCTL,         XXXXXXX,      TO(0),    KC_RGUI
+                                                         KC_LALT,LT(7,KC_ENT),    KC_SPC,         KC_SPC,      TO(0),    KC_RALT
                                                   //`-----------------------------------'    `-----------------------------------'
   ),
     //GameLower
    [7] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------------------------.                            ,-----------------------------------------------------------------------.
-          KC_ESC,       KC_I,      KC_UP,       KC_M,       KC_L,       KC_O,                                 XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+          KC_ESC,       KC_I,      KC_UP,       KC_M,       KC_L,       KC_O,                                 XXXXXXX,      KC_F7,      KC_F8,      KC_F9,     KC_F12,    XXXXXXX,
   //|-----------+-----------+-----------+-----------+-----------+-----------|                            |-----------+-----------+-----------+-----------+-----------+-----------|
-         KC_LSFT,    KC_LEFT,    KC_DOWN,    KC_RGHT,     KC_ENT,       KC_P,                                 XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+         KC_LSFT,    KC_LEFT,    KC_DOWN,    KC_RGHT,     KC_ENT,       KC_P,                                 XXXXXXX,      KC_F4,      KC_F5,      KC_F6,     KC_F11,    XXXXXXX,
   //|-----------+-----------+-----------+-----------+-----------+-----------|                            |-----------+-----------+-----------+-----------+-----------+-----------|
-         KC_LCTL,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                 XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+         KC_LCTL,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                 XXXXXXX,      KC_F1,      KC_F2,      KC_F3,     KC_F10,    XXXXXXX,
   //|-----------+-----------+-----------+-----------+-----------+-----------+-----------|    |-----------+-----------+-----------+-----------+-----------+-----------+-----------|
-                                                         KC_LALT,     KC_SPC,    _______,         XXXXXXX,      TO(0),    XXXXXXX
+                                                         KC_BSPC,    _______,     KC_SPC,         XXXXXXX,      TO(0),    XXXXXXX
                                                   //`-----------------------------------'    `-----------------------------------'
   )
 };
